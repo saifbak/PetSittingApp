c828a3eb20a122982ee234abed0abecfaa140111
AbdullahChauhan <acse.uok@gmail.com>
2021-09-04 18:50:03 +0500

    shared - spacing widget create

---
+++ lib/src/shared/spacing.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class VerticalSpacing extends SizedBox {
+  const VerticalSpacing([double height = 8]) : super(height: height);
+}
+
+class HorizontalSpacing extends SizedBox {
+  const HorizontalSpacing([double width = 8]) : super(width: width);
+}
