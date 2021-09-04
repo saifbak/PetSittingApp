adbb9a4e83fc9fc1d6840dc6a3b0de037e559916
AbdullahChauhan <acse.uok@gmail.com>
2021-09-04 23:49:15 +0500

    splash view init navigate logic fixed, app colors update

--- lib/src/styles/app_colors.dart
+++ lib/src/styles/app_colors.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
+
 class AppColors {
   AppColors._();
+
+  static Color get primaryColor => Color(0xFF46D1B8);
+  static Color get whisper => Color(0xFFE6E6E6);
 }
--- lib/src/views/splash/splash_view.dart
+++ lib/src/views/splash/splash_view.dart
@@ -1,35 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:stacked/stacked.dart';
+import 'package:whiskers_away_app/src/base/utils/utils.dart';
 import 'package:whiskers_away_app/src/services/local/navigation_service.dart';
 import 'package:whiskers_away_app/src/shared/base_view.dart';
 import 'package:whiskers_away_app/src/shared/spacing.dart';
 import 'package:whiskers_away_app/src/views/splash/splash_view_model.dart';
-import 'package:whiskers_away_app/src/base/utils/utils.dart';
 
-class SplashView extends StatefulWidget {
-  @override
-  _SplashViewState createState() => _SplashViewState();
-}
-
-class _SplashViewState extends State<SplashView>
-    with SingleTickerProviderStateMixin {
-  late final Ticker _ticker;
-
+class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<SplashViewModel>.nonReactive(
       viewModelBuilder: () => SplashViewModel(),
       onModelReady: (_) {
-        _ticker = this.createTicker((elapsed) {
-          if (elapsed.inSeconds == 2) NavService.onBoarding();
-        });
-        _ticker.start();
-      },
-      onDispose: (_) {
-        _ticker.dispose();
+        Future.delayed(
+          Duration(seconds: 2),
+          NavService.onBoarding,
+        );
       },
       builder: (_, model, __) {
         return AnnotatedRegion<SystemUiOverlayStyle>(
@@ -61,7 +49,10 @@ class _Body extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/logo_img.png'),
+          Hero(
+            tag: 'logo_text',
+            child: Image.asset('assets/images/logo_img.png'),
+          ),
           VerticalSpacing(screenSize.height * .025),
           Image.asset('assets/images/logo_text.png'),
         ],
