427c33faadcd328686e638635888e2ddbb5cc1cf
AbdullahChauhan <acse.uok@gmail.com>
2021-09-04 20:44:39 +0500

    splash view update (navigate to on boarding view after elapsed time)

--- lib/src/views/splash/splash_view.dart
+++ lib/src/views/splash/splash_view.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:stacked/stacked.dart';
+import 'package:whiskers_away_app/src/services/local/navigation_service.dart';
 import 'package:whiskers_away_app/src/shared/base_view.dart';
 import 'package:whiskers_away_app/src/shared/spacing.dart';
 import 'package:whiskers_away_app/src/views/splash/splash_view_model.dart';
 import 'package:whiskers_away_app/src/base/utils/utils.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
+  @override
+  _SplashViewState createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+  late final Ticker _ticker;
+
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<SplashViewModel>.nonReactive(
       viewModelBuilder: () => SplashViewModel(),
+      onModelReady: (_) {
+        _ticker = this.createTicker((elapsed) {
+          if (elapsed.inSeconds == 2) NavService.onBoarding();
+        });
+        _ticker.start();
+      },
+      onDispose: (_) {
+        _ticker.dispose();
+      },
       builder: (_, model, __) {
         return AnnotatedRegion<SystemUiOverlayStyle>(
           value: SystemUiOverlayStyle.dark.copyWith(
@@ -33,7 +52,6 @@ class SplashView extends StatelessWidget {
 class _Body extends StatelessWidget {
   final SplashViewModel model;
   const _Body(this.model);
-
   @override
   Widget build(BuildContext context) {
     final screenSize = context.screenSize();
