3ab954a59e2318d956a893c9199269dcb012cc12
AbdullahChauhan <acse.uok@gmail.com>
2021-09-06 20:15:04 +0500

    styles - text styles create

---
+++ lib/src/styles/text_styles.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:whiskers_away_app/src/styles/app_colors.dart';
+
+class TextStyles {
+  TextStyles._();
+
+  static TextStyle heading({Color? color}) {
+    final fontColor = color ?? AppColors.primaryColor;
+
+    return TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      color: fontColor,
+    );
+  }
+
+  static TextStyle xxSmall({Color? color, FontWeight? weight}) {
+    final fontColor = color ?? Colors.black;
+    final fontWeight = weight ?? FontWeight.w400;
+
+    return TextStyle(
+      fontSize: 10,
+      fontWeight: fontWeight,
+      color: fontColor,
+    );
+  }
+
+  static TextStyle xSmall({Color? color, FontWeight? weight}) {
+    final fontColor = color ?? Colors.black;
+    final fontWeight = weight ?? FontWeight.w400;
+
+    return TextStyle(
+      fontSize: 11,
+      fontWeight: fontWeight,
+      color: fontColor,
+    );
+  }
+
+  static TextStyle xxMedium({Color? color, FontWeight? weight}) {
+    final fontColor = color ?? Colors.black;
+    final fontWeight = weight ?? FontWeight.w400;
+
+    return TextStyle(
+      fontSize: 12,
+      fontWeight: fontWeight,
+      color: fontColor,
+    );
+  }
+
+  static TextStyle xMedium({Color? color, FontWeight? weight}) {
+    final fontColor = color ?? Colors.black;
+    final fontWeight = weight ?? FontWeight.w400;
+
+    return TextStyle(
+      fontSize: 13,
+      fontWeight: fontWeight,
+      color: fontColor,
+    );
+  }
+
+  static TextStyle xxLarge({Color? color, FontWeight? weight}) {
+    final fontColor = color ?? Colors.black;
+    final fontWeight = weight ?? FontWeight.w400;
+
+    return TextStyle(
+      fontSize: 14,
+      fontWeight: fontWeight,
+      color: fontColor,
+    );
+  }
+
+  static TextStyle xLarge({Color? color, FontWeight? weight}) {
+    final fontColor = color ?? Colors.black;
+    final fontWeight = weight ?? FontWeight.w400;
+
+    return TextStyle(
+      fontSize: 16,
+      fontWeight: fontWeight,
+      color: fontColor,
+    );
+  }
+}
