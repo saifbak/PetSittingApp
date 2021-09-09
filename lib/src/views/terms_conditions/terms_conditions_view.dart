6bb1ea6d0fa913bc104dd0ac6b35d5f8c97a9e70
AbdullahChauhan <acse.uok@gmail.com>
2021-09-10 01:09:35 +0500

    terms conditions view UI complete, view-model update

--- lib/src/views/terms_conditions/terms_conditions_view.dart
+++ lib/src/views/terms_conditions/terms_conditions_view.dart
@@ -6,6 +6,7 @@ import 'package:whiskers_away_app/src/shared/app_button.dart';
 import 'package:whiskers_away_app/src/shared/app_divider.dart';
 import 'package:whiskers_away_app/src/shared/app_top_bar.dart';
 import 'package:whiskers_away_app/src/shared/multi_style_text.dart';
+import 'package:whiskers_away_app/src/shared/read_more_scrollable_content.dart';
 import 'package:whiskers_away_app/src/shared/spacing.dart';
 import 'package:whiskers_away_app/src/styles/app_base_styles.dart';
 import 'package:whiskers_away_app/src/styles/app_colors.dart';
@@ -17,6 +18,7 @@ class TermsConditionsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<TermsConditionsViewModel>.nonReactive(
       viewModelBuilder: () => TermsConditionsViewModel(),
+      onModelReady: (model) {},
       builder: (_, model, __) {
         return Scaffold(
           body: _Body(model),
@@ -46,41 +48,48 @@ class _Body extends StatelessWidget {
           ),
         ),
         VerticalSpacing(16),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 20),
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                offset: Offset(0, 4),
-                blurRadius: 20,
-                color: Colors.black.withOpacity(.06),
-              ),
-            ],
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Terms & Conditions',
-                style: AppTextStyles.large(
-                  color: AppColors.darkGray,
-                  weight: FontWeight.w500,
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 4),
+                  blurRadius: 20,
+                  color: Colors.black.withOpacity(.08),
                 ),
-              ),
-              VerticalSpacing(),
-              AppDivider(),
-              VerticalSpacing(),
-              Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus et scelerisque risus quam porta aenean sed cursus. Vestibulum aliquet eros, euismod platea felis velit. Consectetur arcu proin vel laoreet. \n\nAnte in fames scelerisque ridiculus elementum ornare pellentesque arcu. Volutpat nec nunc, ante in fames scelerisque ridiculus elementum ornare pellentesque arcu. Volutpat nec nunc, aliquet eros, euismod platea felis velit. Consectetur arcu proin vel laoreet. ",
-                style: AppTextStyles.xxLarge(color: Color(0xFF5C5C5C)),
-              )
-            ],
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Terms & Conditions',
+                  style: AppTextStyles.large(
+                    color: AppColors.darkGray,
+                    weight: FontWeight.w500,
+                  ),
+                ),
+                VerticalSpacing(),
+                AppDivider(),
+                VerticalSpacing(),
+                ReadMoreScrollableContent(
+                  contentLength: model.termsDemoData.length,
+                  contentItemBuilder: (_, index) {
+                    return Text(
+                      model.termsDemoData[index],
+                      style: AppTextStyles.xxLarge(color: Color(0xFF5C5C5C)),
+                    );
+                  },
+                )
+              ],
+            ),
           ),
         ),
-        Spacer(),
+        VerticalSpacing(20),
         Padding(
           padding: AppBaseStyles.horizontalPadding,
           child: Column(
@@ -121,8 +130,8 @@ class TermsAgreementCheck extends ViewModelWidget<TermsConditionsViewModel> {
         GestureDetector(
           onTap: () => model.termsAccepted = !model.termsAccepted,
           child: Container(
-            width: 20,
-            height: 20,
+            width: 18,
+            height: 18,
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(4),
@@ -131,7 +140,7 @@ class TermsAgreementCheck extends ViewModelWidget<TermsConditionsViewModel> {
             child: model.termsAccepted
                 ? Icon(
                     Icons.check,
-                    size: 18,
+                    size: 16,
                     color: AppColors.primaryColor,
                   )
                 : SizedBox(),
--- lib/src/views/terms_conditions/terms_conditions_view_model.dart
+++ lib/src/views/terms_conditions/terms_conditions_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:stacked/stacked.dart';
 
 class TermsConditionsViewModel extends BaseViewModel {
@@ -7,4 +8,12 @@ class TermsConditionsViewModel extends BaseViewModel {
     _termsAccepted = val;
     notifyListeners();
   }
+
+  ScrollController scrollController = ScrollController();
+
+  List<String> get termsDemoData => [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus et scelerisque risus quam porta aenean sed cursus.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus et scelerisque risus quam porta aenean sed cursus.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus et scelerisque risus quam porta aenean sed cursus. Vestibulum aliquet eros, euismod platea felis velit. Consectetur arcu proin vel laoreet.\n\nAnte in fames scelerisque ridiculus elementum ornare pellentesque arcu. Volutpat nec nunc, ante in fames scelerisque ridiculus elementum ornare pellentesque arcu. Volutpat nec nunc, aliquet eros, euismod platea felis velit. Consectetur arcu proin vel laoreet.\n\nAnte in fames scelerisque ridiculus elementum ornare pellentesque arcu. Volutpat nec nunc, ante in fames scelerisque ridiculus elementum ornare pellentesque arcu. Volutpat nec nunc, aliquet eros, euismod platea felis velit. Consectetur arcu proin vel laoreet."
+      ];
 }
