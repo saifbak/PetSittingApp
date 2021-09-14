36197ea127c5c9208a871d307a698a8c69512a0f
AbdullahChauhan <acse.uok@gmail.com>
2021-09-14 13:04:52 +0500

    shared - spacing widget update (static bottom spacing add)

--- lib/src/shared/spacing.dart
+++ lib/src/shared/spacing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whiskers_away_app/src/base/utils/utils.dart';
 
 class VerticalSpacing extends SizedBox {
   const VerticalSpacing([double height = 8]) : super(height: height);
@@ -6,4 +7,13 @@ class VerticalSpacing extends SizedBox {
 
 class HorizontalSpacing extends SizedBox {
   const HorizontalSpacing([double width = 8]) : super(width: width);
+}
+
+class AppSpacing {
+  final BuildContext _context;
+
+  AppSpacing(this._context);
+
+  VerticalSpacing get bottomSpacing =>
+      VerticalSpacing(_context.screenSize().height * .035);
 }
