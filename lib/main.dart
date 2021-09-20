3942a375b8048ed095144a562eeed6520bbd3275
saifbak <saif@enorness.com>
2021-09-20 12:14:00 +0500

    app icons added

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:whiskers_away_app/src/app/app_view.dart';
+import 'package:whiskers_away_app/src/base/utils/utils.dart';
+import 'package:whiskers_away_app/src/configs/app_setup.locator.dart';
+import 'package:whiskers_away_app/src/services/local/flavor_service.dart';
+import 'package:package_info/package_info.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // getting package
+  final package = await PackageInfo.fromPlatform();
+
+  setupLocator();
+
+  // setup dialog builders
+  AppUtils.setupCustomDialogBuilders();
+
+  // app flavor init
+  FlavorService.init(package);
+
+  runApp(AppView());
+}
